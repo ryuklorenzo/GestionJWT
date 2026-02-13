eef23acd7f61c696875a6fa4ab81fae65c5ca952
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-02-13 01:20:31 +0100

    Ya funciona la app, que habia explotado tete

--- composeApp/src/commonMain/kotlin/ies/sequeros/dam/pmdm/gestionperifl/di/appModule.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/dam/pmdm/gestionperifl/di/appModule.kt
@@ -1,59 +1,43 @@
 package ies.sequeros.dam.pmdm.gestionperifl.di
 
-import com.russhwolf.settings.Settings
 import ies.sequeros.dam.pmdm.gestionperifl.application.session.SessionManager
 import ies.sequeros.dam.pmdm.gestionperifl.application.usercase.*
 import ies.sequeros.dam.pmdm.gestionperifl.domain.repository.UserRepository
+import ies.sequeros.dam.pmdm.gestionperifl.infraestructure.ktor.createHttpClient
 import ies.sequeros.dam.pmdm.gestionperifl.infraestructure.repository.UserRepositoryImpl
 import ies.sequeros.dam.pmdm.gestionperifl.infraestructure.storage.SettingsTokenStorage
 import ies.sequeros.dam.pmdm.gestionperifl.infraestructure.storage.TokenStorage
+import ies.sequeros.dam.pmdm.gestionperifl.ui.appsettings.AppSettings
 import ies.sequeros.dam.pmdm.gestionperifl.ui.appsettings.AppViewModel
-import ies.sequeros.dam.pmdm.gestionperifl.ui.deleteaccount.DeleteAccountViewModel
-import ies.sequeros.dam.pmdm.gestionperifl.ui.editprofile.EditProfileViewModel
 import ies.sequeros.dam.pmdm.gestionperifl.ui.login.LoginFormViewModel
-import ies.sequeros.dam.pmdm.gestionperifl.ui.password.ChangePasswordViewModel
-import ies.sequeros.dam.pmdm.gestionperifl.ui.principal.HomeFormVIewModel
-import ies.sequeros.dam.pmdm.gestionperifl.ui.profile.ProfileViewModel
 import ies.sequeros.dam.pmdm.gestionperifl.ui.register.RegisterFormViewModel
-import io.ktor.client.HttpClient
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val appModule = module {
-    // 1. Settings (Preferencias)
-    //single { Settings() }
+val appModulo = module {
 
-    // 2. Storage: Decimos que cuando alguien pida TokenStorage, le damos SettingsTokenStorage
-    single<TokenStorage> { SettingsTokenStorage(get()) }
+    single {
+        createHttpClient("http://localhost:8080/api/public/refresh")
+    }
 
-    // 3. Session Manager (recibe TokenStorage)
+    single { SettingsTokenStorage(get()) }
     single { SessionManager(get()) }
+    single<TokenStorage> { get<SettingsTokenStorage>() }
 
-    // 4. HTTP y API
-    //single { HttpClient.client }
-    //single { ApiService(get()) }
+    single<UserRepository> { UserRepositoryImpl(get(), get()) }
 
-    // 5. Repositorios
-    single<UserRepository> { UserRepositoryImpl(get(), get ()) }
+    single { UserRepositoryImpl(get(), get()) }
 
-    // 6. Casos de Uso
+    single { AppSettings() }
     single { LoginUseCase(get()) }
     single { RegisterUseCase(get()) }
-    single { GetProfileUseCase(get()) }
-    single { UpdateUserUseCase(get()) }
     single { ChangePasswordUseCase(get()) }
-    single { DeleteUserUseCase(get()) }
-    single { LogoutUseCase(get(), get()) } // Si existe
+    single { GetProfileUseCase(get()) }
+    single { LogoutUseCase(get(), get()) }
+    single { UpdateUserUseCase(get()) }
 
-    // 7. ViewModels
-    // IMPORTANTE: LoginFormViewModel ahora recibe 2 parámetros: LoginUseCase y SessionManager
+    viewModel { AppViewModel(get()) }
     viewModel { LoginFormViewModel(get(), get()) }
 
     viewModel { RegisterFormViewModel(get()) }
-    //viewModel { HomeFormVIewModel(get()) }
-    viewModel { ProfileViewModel( ) }
-    viewModel { EditProfileViewModel( ) }
-    viewModel { ChangePasswordViewModel( ) }
-    viewModel { DeleteAccountViewModel( ) }
-    viewModel { AppViewModel(get()) }
 }
